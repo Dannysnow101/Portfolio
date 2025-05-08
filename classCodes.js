a6373775f9c84cad3794f17e692cf4bcbffcbd1a
Dannysnow101 <sowaledaniel5@gmail.com>
2025-05-09 00:00:18 +0100

    first commit

---
+++ classCodes.js
@@ -0,0 +1,15 @@
+let arr = ["Apple", "Ball", "Cat", "Dog", "Egg"]
+
+let map = arr.map((e) => {
+    return e;
+});
+
+console.log(map);
+
+let numbers = [1,2,3,4,5,6,7,8,9,10]
+
+let filter = numbers.filter((i) => {
+    return i % 2 !== 0;
+});
+
+console.log(filter);
